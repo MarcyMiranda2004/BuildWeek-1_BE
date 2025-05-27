1ba33b539d129c469f67deb65e0dc19dab130547
Marcello Miranda <marcellomiranda2004@gmail.com>
2025-05-27 17:19:30 +0200

    Fix Utente

--- src/main/java/entities/Amministratore.java
+++ src/main/java/entities/Amministratore.java
@@ -1,37 +1,23 @@
+// Classe Amministratore aggiornata
 package entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Amministratore extends Utente {
 
-    @Column(length = 20, nullable = false)
-    private String username;
-
-    @Column(length = 20, nullable = false)
-    private String password;
-
     public Amministratore() {}
 
     public Amministratore(String nome, String cognome, String username, String password) {
-        super(nome, cognome);
-        this.username = username;
-        this.password = password;
+        super(nome, cognome, username, password);
     }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 
     @Override
     public String toString() {
         return "Amministratore{" +
                 "nome='" + getNome() + '\'' +
                 ", cognome='" + getCognome() + '\'' +
-                ", username='" + username + '\'' +
+                ", username='" + getUsername() + '\'' +
                 '}';
     }
 }
